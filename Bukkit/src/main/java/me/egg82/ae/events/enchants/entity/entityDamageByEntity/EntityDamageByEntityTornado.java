0b560da05fa513efb682f805ba1c37d2884df9dc
egg82 <eggys82@gmail.com>
2019-06-30 21:05:37 -0600

    Added tornado enchant, slight nerf to rampage

--- Bukkit/src/main/java/me/egg82/ae/events/enchants/entity/entityDamageByEntity/EntityDamageByEntityTornado.java
+++ Bukkit/src/main/java/me/egg82/ae/events/enchants/entity/entityDamageByEntity/EntityDamageByEntityTornado.java
@@ -7,22 +7,28 @@ import me.egg82.ae.EnchantAPI;
 import me.egg82.ae.api.AdvancedEnchantment;
 import me.egg82.ae.api.BukkitEnchantableItem;
 import me.egg82.ae.api.GenericEnchantableItem;
-import me.egg82.ae.services.CollectionProvider;
 import me.egg82.ae.services.entity.EntityItemHandler;
 import ninja.egg82.service.ServiceLocator;
 import ninja.egg82.service.ServiceNotFoundException;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.util.Vector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EntityDamageByEntityRampage implements Consumer<EntityDamageByEntityEvent> {
+public class EntityDamageByEntityTornado implements Consumer<EntityDamageByEntityEvent> {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private EnchantAPI api = EnchantAPI.getInstance();
 
-    public EntityDamageByEntityRampage() { }
+    private final Plugin plugin;
+
+    public EntityDamageByEntityTornado(Plugin plugin) {
+        this.plugin = plugin;
+    }
 
     public void accept(EntityDamageByEntityEvent event) {
         EntityItemHandler entityItemHandler;
@@ -41,8 +47,8 @@ public class EntityDamageByEntityRampage implements Consumer<EntityDamageByEntit
         boolean hasEnchantment;
         int level;
         try {
-            hasEnchantment = api.anyHasEnchantment(AdvancedEnchantment.RAMPAGE, enchantableMainHand);
-            level = api.getMaxLevel(AdvancedEnchantment.RAMPAGE, enchantableMainHand);
+            hasEnchantment = api.anyHasEnchantment(AdvancedEnchantment.TORNADO, enchantableMainHand);
+            level = api.getMaxLevel(AdvancedEnchantment.TORNADO, enchantableMainHand);
         } catch (APIException ex) {
             logger.error(ex.getMessage(), ex);
             return;
@@ -52,13 +58,10 @@ public class EntityDamageByEntityRampage implements Consumer<EntityDamageByEntit
             return;
         }
 
-        Integer value = CollectionProvider.getRampage().get(event.getDamager().getUniqueId());
-        if (value == null) {
-            return;
-        }
-
-        double damage = event.getDamage();
-        damage += damage - (damage / (level * value + 0.75d));
-        event.setDamage(damage);
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            if (!event.getEntity().isDead()) {
+                event.getEntity().setVelocity(new Vector(0.0d, 0.35d * level, 0.0d));
+            }
+        }, 1L);
     }
 }
