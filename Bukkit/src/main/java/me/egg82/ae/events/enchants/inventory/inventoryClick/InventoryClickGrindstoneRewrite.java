1bea2a31595c46ea17d755885683514973af741a
egg82 <eggys82@gmail.com>
2019-07-09 21:53:52 -0600

    Grindstone now gives levels

--- Bukkit/src/main/java/me/egg82/ae/events/enchants/inventory/inventoryClick/InventoryClickGrindstoneRewrite.java
+++ Bukkit/src/main/java/me/egg82/ae/events/enchants/inventory/inventoryClick/InventoryClickGrindstoneRewrite.java
@@ -2,15 +2,20 @@ package me.egg82.ae.events.enchants.inventory.inventoryClick;
 
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import java.util.function.Consumer;
+import me.egg82.ae.api.AdvancedEnchantment;
 import me.egg82.ae.api.BukkitEnchantableItem;
 import me.egg82.ae.api.GenericEnchantment;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class InventoryClickGrindstoneRewrite implements Consumer<InventoryClickEvent> {
+    private Random rand = new Random();
+
     public InventoryClickGrindstoneRewrite() { }
 
     public void accept(InventoryClickEvent event) {
@@ -21,19 +26,27 @@ public class InventoryClickGrindstoneRewrite implements Consumer<InventoryClickE
         }
 
         BukkitEnchantableItem enchantableResultItem = BukkitEnchantableItem.fromItemStack(resultItem);
-        removeEnchants(enchantableResultItem);
+        int maxExp = removeEnchants(enchantableResultItem);
+        int variation = maxExp - (maxExp / 4);
 
         enchantableResultItem.rewriteMeta();
         event.setCurrentItem((ItemStack) enchantableResultItem.getConcrete());
+
+        if (event.getWhoClicked() instanceof Player) {
+            ((Player) event.getWhoClicked()).giveExp(rand.nextInt(maxExp + variation) + (maxExp - variation));
+        }
     }
 
-    private void removeEnchants(BukkitEnchantableItem enchantableResultItem) {
+    private int removeEnchants(BukkitEnchantableItem enchantableResultItem) {
         Set<GenericEnchantment> removedEnchants = new HashSet<>();
+        int retVal = 0;
         for (Map.Entry<GenericEnchantment, Integer> kvp : enchantableResultItem.getEnchantments().entrySet()) {
-            if (!kvp.getKey().isCurse()) {
+            if (kvp.getKey() instanceof AdvancedEnchantment && !kvp.getKey().isCurse()) {
                 removedEnchants.add(kvp.getKey());
+                retVal += 6 * kvp.getValue();
             }
         }
         enchantableResultItem.removeEnchantments(removedEnchants);
+        return retVal;
     }
 }
