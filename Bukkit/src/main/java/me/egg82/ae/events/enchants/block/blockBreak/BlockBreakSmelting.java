181e6aef760fe903224efa32d1da598ff6547879
egg82 <eggys82@gmail.com>
2019-07-14 10:33:38 -0600

    Fixed block duplication with smelting

--- Bukkit/src/main/java/me/egg82/ae/events/enchants/block/blockBreak/BlockBreakSmelting.java
+++ Bukkit/src/main/java/me/egg82/ae/events/enchants/block/blockBreak/BlockBreakSmelting.java
@@ -92,9 +92,7 @@ public class BlockBreakSmelting implements Consumer<BlockBreakEvent> {
                 break;
             }
 
-            if (!dropped) {
-                event.getPlayer().getWorld().dropItemNaturally(dropLoc, i);
-            } else {
+            if (dropped) {
                 isSmelted = true;
             }
         }
