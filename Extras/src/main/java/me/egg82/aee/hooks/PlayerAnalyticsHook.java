93473b99acfdedf68ea860c9f2859361bef82909
egg82 <eggys82@gmail.com>
2019-11-17 12:54:16 -0700

    More pom fixes

--- Extras/src/main/java/me/egg82/aee/hooks/PlayerAnalyticsHook.java
+++ Extras/src/main/java/me/egg82/aee/hooks/PlayerAnalyticsHook.java
@@ -1,39 +1,100 @@
 package me.egg82.aee.hooks;
 
-import com.djrapitops.plan.api.PlanAPI;
-import com.djrapitops.plan.data.element.AnalysisContainer;
-import com.djrapitops.plan.data.element.InspectContainer;
-import com.djrapitops.plan.data.plugin.ContainerSize;
-import com.djrapitops.plan.data.plugin.PluginData;
-import com.djrapitops.plan.utilities.html.icon.Color;
-import com.djrapitops.plan.utilities.html.icon.Icon;
-import java.util.Collection;
-import java.util.UUID;
+import com.djrapitops.plan.capability.CapabilityService;
+import com.djrapitops.plan.extension.CallEvents;
+import com.djrapitops.plan.extension.DataExtension;
+import com.djrapitops.plan.extension.ExtensionService;
+import com.djrapitops.plan.extension.annotation.PluginInfo;
+import com.djrapitops.plan.extension.icon.Color;
+import com.djrapitops.plan.extension.icon.Family;
+import java.util.*;
 import me.egg82.ae.EnchantAPI;
+import me.egg82.ae.api.*;
+import me.egg82.ae.hooks.PluginHook;
+import me.egg82.ae.services.entity.EntityItemHandler;
+import ninja.egg82.service.ServiceLocator;
+import ninja.egg82.service.ServiceNotFoundException;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PlayerAnalyticsHook implements PluginHook {
     private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final CapabilityService capabilities;
 
-    public PlayerAnalyticsHook() { PlanAPI.getInstance().addPluginDataSource(new Data()); }
+    public PlayerAnalyticsHook() {
+        capabilities = CapabilityService.getInstance();
 
-    public void cancel() {}
+        if (isCapabilityAvailable("DATA_EXTENSION_VALUES")) {
+            try {
+                ExtensionService.getInstance().register(new Data());
+            } catch (NoClassDefFoundError ex) {
+                // Plan not installed
+                logger.error("Plan is not installed.", ex);
+            } catch (IllegalStateException ex) {
+                // Plan not enabled
+                logger.error("Plan is not enabled.", ex);
+            } catch (IllegalArgumentException ex) {
+                // DataExtension impl error
+                logger.error("DataExtension implementation exception.", ex);
+            }
+        }
+    }
 
-    class Data extends PluginData {
+    public void cancel() { }
+
+    private boolean isCapabilityAvailable(String capability) {
+        try {
+            return capabilities.hasCapability(capability);
+        } catch (NoClassDefFoundError ignored) {
+            return false;
+        }
+    }
+
+    @PluginInfo(
+            name = "AdvancedEnchantments-Extras",
+            iconName = "book",
+            iconFamily = Family.SOLID,
+            color = Color.PURPLE
+    )
+    class Data implements DataExtension {
         private final EnchantAPI api = EnchantAPI.getInstance();
+        private final CallEvents[] events = new CallEvents[] { };
+        private EntityItemHandler entityItemHandler;
+        private final BukkitEnchantment durability = BukkitEnchantment.fromEnchant(Enchantment.DURABILITY);
 
         private Data() {
-            super(ContainerSize.THIRD, "AdvancedEnchantments");
-            setPluginIcon(Icon.called("book").of(Color.PURPLE).build());
+            try {
+                entityItemHandler = ServiceLocator.get(EntityItemHandler.class);
+            } catch (InstantiationException | IllegalAccessException | ServiceNotFoundException ex) {
+                logger.error(ex.getMessage(), ex);
+            }
         }
 
-        public InspectContainer getPlayerData(UUID uuid, InspectContainer container) {
-            return container;
+        private Set<GenericEnchantableItem> getItems(Player player) {
+            Set<GenericEnchantableItem> retVal = new HashSet<>();
+
+            Optional<ItemStack> mainHand = entityItemHandler.getItemInMainHand(player);
+            mainHand.ifPresent(itemStack -> retVal.add(BukkitEnchantableItem.fromItemStack(itemStack)));
+            Optional<ItemStack> offHand = entityItemHandler.getItemInOffHand(player);
+            offHand.ifPresent(itemStack -> retVal.add(BukkitEnchantableItem.fromItemStack(itemStack)));
+
+            Optional<EntityEquipment> equipment = Optional.ofNullable(player.getEquipment());
+            if (!equipment.isPresent()) {
+                return retVal;
+            }
+
+            retVal.add(BukkitEnchantableItem.fromItemStack(equipment.get().getHelmet()));
+            retVal.add(BukkitEnchantableItem.fromItemStack(equipment.get().getChestplate()));
+            retVal.add(BukkitEnchantableItem.fromItemStack(equipment.get().getLeggings()));
+            retVal.add(BukkitEnchantableItem.fromItemStack(equipment.get().getBoots()));
+            retVal.remove(null);
+            return retVal;
         }
 
-        public AnalysisContainer getServerData(Collection<UUID> uuids, AnalysisContainer container) {
-            return container;
-        }
+        public CallEvents[] callExtensionMethodsOn() { return events; }
     }
 }
